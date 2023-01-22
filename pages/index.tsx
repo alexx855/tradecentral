38f03c1cc80d7219e73ccfade3116988f5121111
Alex Pedersen <mails@alexpedersen.dev>
2023-01-22 17:00:18 +0000

    Add trade and user pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import { ReactElement } from 'react';
 import Layout from '../components/Layouts/layout';
+import TradeItem from '../components/Trade/Trade';
+import { DUMMY_TRADE_ITEMS } from './trade/[tid]';
 import { NextPageWithLayout } from './_app';
 
 interface HomeProps {
@@ -7,9 +9,15 @@ interface HomeProps {
 
 const HomePage: NextPageWithLayout = (props: HomeProps) => {
   return (
-    <div className='p-3'>
-      TODO: define home page content,  list all available trades?
-    </div>
+    <section className="p-8">
+      <div className="mx-auto max-w-screen-sm text-center mb-8">
+        <h2 className="mb-3 text-3xl lg:text-3xl tracking-tight font-extrabold text-gray-900 ">Open Trades</h2>
+        <p className="font-light text-gray-500 sm:text-xl">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus, iure quod? Obcaecati ipsa voluptate eos cumque, ex amet sunt quia tempore sapiente et id, optio aut repellat ad? Sed, maiores?</p>
+      </div>
+      <div className="grid gap-8 lg:grid-cols-2">
+        {DUMMY_TRADE_ITEMS.map((item) => (<TradeItem showLink showUser {...item} key={item.id} />))}
+      </div>
+    </section>
   );
 };
 
@@ -17,8 +25,8 @@ HomePage.getLayout = function getLayout(page: ReactElement) {
   return (<Layout>{page}</Layout>)
 }
 
-
 HomePage.getInitialProps = async (ctx) => {
   return {}
 }
+
 export default HomePage;
