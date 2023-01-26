63b7c4368ec9fe4d4372ca3755aaa09d66ab90a1
tomi204 <tomi204@outlook.com>
2023-01-25 22:50:25 -0300

    form delete

--- components/TradeModal/TradeModal.jsx
+++ components/TradeModal/TradeModal.jsx
@@ -87,7 +87,7 @@ if(isSuccess){
                       </button>
                   </div>
                   
-                  <form action="#">
+                  <form >
                       <div className="grid gap-4 mb-4 sm:grid-cols-2">
                           <div>
                               <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"/>Name
@@ -116,7 +116,7 @@ if(isSuccess){
                               <textarea onChange={(e) => setDesc(e.target.value)} id="description" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Write product description here"></textarea>                    
                           </div>
                       </div>
-                      <button onClick={() => write?.()} type="submit" className="text-white inline-flex items-center bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+                      <button onClick={() => write?.()}  className="text-white inline-flex items-center bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
                     <svg className="mr-1 -ml-1 w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path></svg>
                           Add new product
                       </button>
