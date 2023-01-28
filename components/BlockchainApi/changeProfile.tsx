38e3e74d9e98080d4e97e28227732867c472de04
tomas <tomi204@outlook.com>
2023-01-27 21:43:58 -0300

    update profile

---
+++ components/BlockchainApi/changeProfile.tsx
@@ -0,0 +1,95 @@
+import React from "react";
+import { useAccount, useContractWrite, usePrepareContractWrite } from "wagmi";
+import { useState } from "react";
+
+const CONTRACT_ADDRESS = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS
+
+export const ChangeProfile = () => {
+  const { address, isConnected } = useAccount();
+  const [email, setEmail] = useState("");
+  const [tokenURI, setTokenURI] = useState("");
+  const [name, setName] = useState("");
+
+
+  const { config } = usePrepareContractWrite({
+    address: CONTRACT_ADDRESS,
+    overrides: {
+      from: address,
+      // gasLimit: 1000000,
+    },
+    abi: [
+    	{
+				"inputs": [
+					{
+						"internalType": "string",
+						"name": "_email",
+						"type": "string"
+					},
+					{
+						"internalType": "string",
+						"name": "_name",
+						"type": "string"
+					},
+					{
+						"internalType": "string",
+						"name": "_image",
+						"type": "string"
+					}
+				],
+				"name": "updateProfile",
+				"outputs": [],
+				"stateMutability": "nonpayable",
+				"type": "function"
+			},
+    ],
+    args: [email, name, tokenURI],
+    functionName: "updateProfile",
+  });
+  const { write } = useContractWrite(config);
+
+
+  if (!isConnected) {
+    return (
+      <div className="flex flex-col items-center justify-center">
+        <span className=" text-2xl font-bold text-gray-500">
+          Please connect your wallet
+        </span>
+      </div>
+    );
+  }
+
+  return (
+    <>
+
+      <div className=" flex flex-col items-center justify-center" >
+        <div className="">
+
+          <input className=""
+            placeholder="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input className=""
+            placeholder="name"
+            onChange={(e) => setName(e.target.value)}
+          />
+          {/* TODO: implement images storage with lighthouse SKD */}
+          {/* <input className=""
+            placeholder="profile image"
+            onChange={(e) => setTokenURI(e.target.value)}
+          /> */}
+          <br />
+          {/* address form */}
+
+          <button
+            onClick={() => write?.()}
+          >
+            Update profile info
+          </button>
+        </div>
+      </div>
+
+    </>
+  );
+};
+
+export default ChangeProfile;
