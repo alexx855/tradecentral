6c05b99b1e6640ec241b8abb5fc1b0b0b0490c1a
Alex Pedersen <mails@alexpedersen.dev>
2023-02-03 18:45:14 +0000

    Update utils

--- utils.ts
+++ utils.ts
@@ -1,5 +1,6 @@
-// remplace white spaces and any other character that are not a letter or a number by a dash
 export function normalizeString(input: string): string {
+  // replace white spaces and any other character that are not a letter or a number by a dash
   const output = input.replace(/[^a-zA-Z0-9]/g, '-').toLowerCase();
-  return output;
-}
+  // replace multiple dashes by one dash and remove dashes at the beginning and at the end
+  return output.replace(/-+/g, '-').replace(/^-+/, '').replace(/-+$/, '');
+} 
