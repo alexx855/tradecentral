48129b8ed1895521f49753080de98816f97711bb
tomas <tomi204@outlook.com>
2023-02-04 16:37:30 -0300

    staking funtion

---
+++ components/BlockchainApi/Staking.tsx
@@ -0,0 +1,62 @@
+import { BigNumber, ethers, utils } from "ethers";
+import { formatEther } from "ethers/lib/utils.js";
+import React from "react";
+import { useAccount, useContractWrite, usePrepareContractWrite } from "wagmi";
+import { GetAllItems } from "./ListedTokens";
+const CONTRACT_ADDRESS = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS
+const CHAIN_ID = process.env.NEXT_PUBLIC_CHAIN_ID
+
+
+const Staking = (id: any) => {
+  const { trades } = GetAllItems();
+  const { address, isConnected } = useAccount();
+  const { config } = usePrepareContractWrite({
+    address: CONTRACT_ADDRESS,
+    chainId: +CHAIN_ID!,
+    overrides: {
+      from: address,
+      gasLimit: BigNumber.from(30000000),
+    },
+    functionName: "staking",
+    abi: [
+   {
+				"inputs": [
+					{
+						"internalType": "uint256",
+						"name": "_itemId",
+						"type": "uint256"
+					}
+				],
+				"name": "staking",
+				"outputs": [],
+				"stateMutability": "nonpayable",
+				"type": "function"
+			},
+    ],
+    args: [id.id],
+    onError: (error) => {
+      console.log(error);
+    },
+    onSuccess: (data) => {
+      console.log(data);
+    },
+  });
+  const { data, isLoading, isSuccess, write, isError } = useContractWrite(config);
+
+  // TODO: do some checks here
+  // if (isError) {
+  return (
+    <button
+      disabled={!isConnected || isLoading}
+      onClick={() => write?.()}
+      className="text-white bg-blue-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+    >
+      {isLoading ? (
+        'Check Wallet'
+      ) : isSuccess ? ('Bought') : 'Confirm and send money to the seller'}
+    </button>
+  );
+
+};
+
+export default Staking;
