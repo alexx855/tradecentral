6ba6cfa204dddee96a90d11b71c794abf3a15242
Alex Pedersen <mails@alexpedersen.dev>
2023-02-05 20:48:13 +0100

    Add export command for spheron

--- next.config.js
+++ next.config.js
@@ -4,8 +4,18 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: false,
   images: {
+    unoptimized: true,
     domains: ['gateway.lighthouse.storage', "i.imgur.com"],
   },
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      "/": { page: "/" },
+      "/404": { page: "/404" },
+    };
+  }
 }
 
 module.exports = nextConfig;
